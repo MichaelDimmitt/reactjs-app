5e69be0532f5909a96a912bda0cfe1c498bac50e
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-04-18 19:12:53 -0400

    put example html and component in return of render function. fix quotes

--- src/App.js
+++ src/App.js
@@ -11,36 +11,32 @@ const Footero= (props) => {
   )
 }
 
-
-
-const Contact_row = ({a,b,c,}) => {
+const ContactRow = ({a,b,c,d}) => {
 	return (<div>
 		<li class={a}>
 			<i class={b}></i>
-			<a href={c}/>
+			<a href={c}>{d}</a>
 		</li>
 	</div>)
 }
-<div class="contact-container container-block">
-  <ul class="list-unstyled contact-list">
-    <Contact_row a='email' b='fa fa-envelope' c='mailto: volodymyr.pliuta@vladev.net">volodymyr.pliuta@vladev.net'/>
-    <li class=""><i class=""></i><a href="</a></li>
-    <li class="phone"><i class="fa fa-phone"></i><a href="tel:904 383 6783">904 383 6783</a></li>
-    <li class="github"><i class="fa fa-github"></i><a href="https://github.com/americanman">GitHub</a></li>
-    <li class="linkedin"><i class="fa fa-linkedin"></i><a href="https://www.linkedin.com/in/volodymyr-pliuta-40a888115" target="_blank">LinkedIn.com</a></li>
- </ul>
-</div><!--//contact-container-
 
 const Appo= (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to OpenHax</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Welcome to OpenHax</h1>
       </header>
-      <p className="App-intro">
+      <p className='App-intro'>
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
+      <div class='contact-container container-block'>
+        <ul class='list-unstyled contact-list'>
+          <ContactRow a='email' b='fa fa-envelope' c='mailto: volodymyr.pliuta@vladev.net' d='volodymyr.pliuta@vladev.net'/>
+          <li class='phone'><i class='fa fa-phone'></i><a href='tel:904 383 6783'>904 383 6783</a></li>
+          <li class='github'><i class='fa fa-github'></i><a href='https://github.com/americanman'>GitHub</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
