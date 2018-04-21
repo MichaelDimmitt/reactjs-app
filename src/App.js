7a12c80b58b172d25f36e48f0c3361fd46d501dc
americanman <snowboardracer7@gmail.com>
2018-04-21 15:20:47 -0400

    added three components to contact section

--- src/App.js
+++ src/App.js
@@ -14,13 +14,29 @@ const Footero= (props) => {
 }
 
 const ContactRow = ({a,b,c,d}) => {
-	return (<div>
-		<li class={a}>
-			<i class={b}></i>
-			<a href={c}>{d}</a>
-		</li>
-	</div>)
+  return (
+    <div>
+		  <li class={a}>
+		  	<i class={b}></i>
+		  	<a href={c}>{d}</a>
+		  </li>
+    </div>
+  )
 }
+
+const ContactContainer = (props) => {
+  return (
+    <div class='contact-container container-block'>
+      <ul class='list-unstyled contact-list'>
+        <ContactRow a='email' b='fa fa-envelope' c='mailto: volodymyr.pliuta@vladev.net' d='volodymyr.pliuta@vladev.net'/>
+        <ContactRow  a='phone' b='fa fa-phone' c='tel:904 383 6783' d='904 383 6783'/>
+        <ContactRow a='github' b='fa fa-github' c='https://github.com/americanman' d='GitHub'/>
+        <ContactRow a='linkedin' b='fa fa-linkedin' c='https://www.linkedin.com/in/volodymyr-pliuta-40a888115' d='LinkedIn'/>
+      </ul>
+    </div>
+  )
+}
+
 
 const Appo= (props) => {
   return (
@@ -32,13 +48,7 @@ const Appo= (props) => {
       <p className='App-intro'>
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
-      <div class='contact-container container-block'>
-        <ul class='list-unstyled contact-list'>
-          <ContactRow a='email' b='fa fa-envelope' c='mailto: volodymyr.pliuta@vladev.net' d='volodymyr.pliuta@vladev.net'/>
-          <li class='phone'><i class='fa fa-phone'></i><a href='tel:904 383 6783'>904 383 6783</a></li>
-          <li class='github'><i class='fa fa-github'></i><a href='https://github.com/americanman'>GitHub</a></li>
-        </ul>
-      </div>
+      <ContactContainer/>
     </div>
   )
 }
