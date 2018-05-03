5d56bdd42049dda4ee8d4d7164fd58ab45d519d6
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-05-02 20:00:21 -0400

    give a few different examples of stateless component usage. Additionally, improve variable names for readability.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import './style.css';
 import 'font-awesome/css/font-awesome.min.css';
-import Appo from './Appo'
 
-//const Footero = ({namedContent}) => {
-const Footero= (props) => {
+// stateless component that makes use of 
+// dereferencing the props object, into two variables.
+const FooterComponent = ({namedContent, additionalContent}) => {
   return (
     <div>
-      {props.namedContent}
+      {namedContent}
     </div>
   )
 }
@@ -19,14 +19,32 @@ const Footero= (props) => {
 //     </div>
 //   )
 // }
+// stateless component that makes use of the props object.
+// also {} allow you to dereference html to javascript 
+// inside a return of html to a render function or inside a render function.
+const LandingPage= (props) => {
+  console.log(props)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={props.logo} className="App-logo" alt="logo" />
+        {/*logo is not passed in this circumstance so we are leaving it out.*/}
+        <h1 className="App-title">{props.propExample}</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/*<Headero/>*/}
-        <Appo/>
-        <Footero namedContent='test passed'/>
+        {/*<HeaderComponent/>*/}
+        <LandingPage logo={logo}  propExample='prop example' />
+        <FooterComponent namedContent='test passed' additionalContent='additional information'/>
       </div>
     );
   }
